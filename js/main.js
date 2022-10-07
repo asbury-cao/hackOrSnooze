b767e0735426b9f47751e304a977a527a736cc0f
Ashley Mathew <amathew195@gmail.com>
2022-10-06 19:18:03 -0500

    added global variables for submit story form inputs

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
 const $navSubmitStory = $("#nav-submit");
 
+const $authorInput = $("#authorInput");
+const $urlInput = $("#urlInput");
+const $titleInput = $("#titleInput");
+
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
